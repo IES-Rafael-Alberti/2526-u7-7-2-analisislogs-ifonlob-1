5bbc9e856d839a5677845fa7564ee31abbad624a
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-08 17:53:58 +0200

    Lógica de composición añadida

--- src/main/kotlin/LogProcessor.kt
+++ src/main/kotlin/LogProcessor.kt
@@ -1,4 +1,7 @@
 package org.iesra
 
-class LogProcessor {
+class LogProcessor (private val parser : LogParser = LogParser(), private val filter : LogFilter = LogFilter(), private val stats : LogStats = LogStats()){
+    fun ejecutarProceso(lineasBrutas : List<String>){
+
+    }
 }
