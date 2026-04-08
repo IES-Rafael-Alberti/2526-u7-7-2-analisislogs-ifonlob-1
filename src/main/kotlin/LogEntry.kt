4d16fe3c03e85505b89058f53a6b9cdee8cf35a5
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-08 17:54:38 +0200

    Añadida clase estructura de logs

---
+++ src/main/kotlin/LogEntry.kt
@@ -0,0 +1,10 @@
+package org.iesra
+
+import java.time.LocalDateTime
+
+data class LogEntry(
+    val timestamp: LocalDateTime,
+    val level: LogLevel,
+    val message: String,
+    val isValid: Boolean = true
+)
