7a82f948d39a81bd34e42ac66c85492597c2268b
ifonlob <ifonlob1108@g.educaand.es>
2026-04-09 10:37:18 +0200

    Métodos de estadísticas completados

--- src/main/kotlin/LogStats.kt
+++ src/main/kotlin/LogStats.kt
@@ -1,4 +1,16 @@
 package org.iesra
 
 class LogStats {
+    fun lineasProcesadas(logs : List<LogEntry>) = logs.size
+    fun lineasValidas(logs : List<LogEntry>) = logs.filter{linea -> linea.isValid}
+    fun lineasInvalidas(logs : List<LogEntry>) = logs.filter{linea -> !linea.isValid}
+    fun contadorInfo(logs : List<LogEntry>) = logs.count{linea -> linea.level == LogLevel.INFO}
+    fun contadorWarning(logs : List<LogEntry>) = logs.count{linea -> linea.level == LogLevel.WARNING}
+    fun contadorError(logs : List<LogEntry>) = logs.count{linea -> linea.level == LogLevel.ERROR}
+    fun primeraFecha(logs : List<LogEntry>) = logs.minBy {linea-> linea.timestamp}
+    fun ultimaFecha(logs : List<LogEntry>) = logs.maxBy {linea-> linea.timestamp}
+
+
+
+
 }
