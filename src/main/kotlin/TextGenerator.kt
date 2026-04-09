18e5da9cc109d504a078ab524bf1f7d982e75379
ifonlob <ifonlob1108@g.educaand.es>
2026-04-09 13:10:46 +0200

    Método de generaciónd e estadísticas hecho

---
+++ src/main/kotlin/TextGenerator.kt
@@ -0,0 +1,56 @@
+package org.iesra
+
+import java.time.LocalDateTime
+
+class TextGenerator(private val stats : LogStats = LogStats(), private val logs : List<LogEntry>, private val fileName : String, private val comienzo : LocalDateTime?, private val final : LocalDateTime?) {
+    fun generarEstadisticas(rango : String = calcularRango()) : String{
+        val estadisticas = buildString {
+            appendLine("INFORME DE LOGS")
+            appendLine("===============")
+            appendLine("Fichero analizado: $fileName")
+            appendLine("Rango aplicado: ${calcularRango()}")
+            appendLine("Niveles incluidos: ${nivelesIncluidos()}")
+            appendLine()
+            appendLine("Resumen:")
+            appendLine("- Líneas procesadas: ${stats.lineasProcesadas(logs)}")
+            appendLine("- Líneas válidas: ${stats.lineasValidas(logs)}")
+            appendLine("- Líneas inválidas: ${stats.lineasInvalidas(logs)}")
+            appendLine()
+            appendLine("Conteo por nivel:")
+            appendLine("- INFO: ${stats.contadorInfo(logs)}")
+            appendLine("- WARNING: ${stats.contadorWarning(logs)}")
+            appendLine("- ERROR: ${stats.contadorError(logs)}")
+            appendLine()
+            appendLine("Periodo detectado:")
+            appendLine("- Primera entrada: ${stats.primeraFecha(logs)}")
+            appendLine("- Primera entrada: ${stats.ultimaFecha(logs)}")
+        }
+        return estadisticas
+    }
+
+    fun calcularRango() : String{
+        return if(comienzo == null){
+            "$final y anteriores"
+        }
+        else if(final == null){
+            "$comienzo y posteriores"
+        }
+        else{
+            "$comienzo - $final"
+        }
+    }
+
+    fun nivelesIncluidos() : Set<LogLevel>{
+        val niveles : MutableList<LogLevel> = mutableListOf()
+        if(stats.contadorInfo(logs) >= 1) {
+            niveles.add(LogLevel.INFO)
+        }
+        if(stats.contadorError(logs) >= 1) {
+            niveles.add(LogLevel.ERROR)
+        }
+        if(stats.contadorWarning(logs) >= 1) {
+            niveles.add(LogLevel.WARNING)
+        }
+        return niveles.toSet()
+    }
+}
