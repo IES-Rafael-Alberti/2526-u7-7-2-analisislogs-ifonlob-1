51f03d409bf7995d7151c8f1cd0692bc98f3232c
ifonlob <ifonlob1108@g.educaand.es>
2026-04-09 10:22:56 +0200

    Método de filtrado de fechas añadido

--- src/main/kotlin/LogFilter.kt
+++ src/main/kotlin/LogFilter.kt
@@ -1,4 +1,22 @@
 package org.iesra
+import java.time.LocalDateTime
 
 class LogFilter {
+    fun filtrar(logs : List<LogEntry>, comienzo : LocalDateTime?, final : LocalDateTime?) : List<LogEntry>{
+        if (comienzo == null){
+            return logs.filter{log ->
+                log.timestamp < final
+            }
+        }
+        else if(final == null){
+            return logs.filter{log ->
+                log.timestamp > comienzo
+            }
+        }
+        else{
+            return logs.filter{log ->
+                log.timestamp > comienzo && log.timestamp < final
+            }
+        }
+    }
 }
