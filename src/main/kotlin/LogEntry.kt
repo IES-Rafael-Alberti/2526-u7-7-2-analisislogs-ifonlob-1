f3a00fb6750ed7e6b1c4c9a090c995f35683d379
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-09 16:03:34 +0200

    Método parsear añadido

--- src/main/kotlin/LogEntry.kt
+++ src/main/kotlin/LogEntry.kt
@@ -3,8 +3,8 @@ package org.iesra
 import java.time.LocalDateTime
 
 data class LogEntry(
-    val timestamp: LocalDateTime,
-    val level: LogLevel,
+    val timestamp: LocalDateTime?,
+    val level: LogLevel?,
     val message: String,
     val isValid: Boolean = true
 )
