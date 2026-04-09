bf6f47e4dc82336c69f98b248da3175a774c0a63
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-09 16:21:54 +0200

    Implementando método ejecutar proceso (recibe parámetros de línea de comando)

--- src/main/kotlin/LogProcessor.kt
+++ src/main/kotlin/LogProcessor.kt
@@ -1,7 +1,16 @@
 package org.iesra
+import java.time.LocalDateTime
 
 class LogProcessor (private val parser : LogParser = LogParser(), private val filter : LogFilter = LogFilter(), private val stats : LogStats = LogStats()){
-    fun ejecutarProceso(lineasBrutas : List<String>){
-
+    fun ejecutarProceso(lineasBrutas: List<String>, nombreArchivo: String, fechaInicio: LocalDateTime?, fechaFin: LocalDateTime?, soloEstadisticas: Boolean, salida: Output){
+        val logsParseados = parser.parsear(lineasBrutas)
+        val logsFiltrados = filter.filtrar(logsParseados, fechaInicio, fechaFin)
+        val generador = TextGenerator(stats, logsFiltrados, nombreArchivo, fechaInicio, fechaFin)
+        val contenidoFinal = if (soloEstadisticas) {
+            generador.generarEstadisticas()
+        } else {
+            generador.generarReporte()
+        }
+        salida.escribirDatos(contenidoFinal)
     }
 }
