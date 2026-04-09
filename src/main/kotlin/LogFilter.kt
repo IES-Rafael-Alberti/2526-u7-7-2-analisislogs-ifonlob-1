93baf621cbf1fe424c88ceba69cd7131144d682b
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-09 16:54:25 +0200

    Lógica de filtrado adaptado a nullables

--- src/main/kotlin/LogFilter.kt
+++ src/main/kotlin/LogFilter.kt
@@ -3,20 +3,21 @@ import java.time.LocalDateTime
 
 class LogFilter {
     fun filtrar(logs : List<LogEntry>, comienzo : LocalDateTime?, final : LocalDateTime?) : List<LogEntry>{
-        return if (comienzo == null){
-            logs.filter{log ->
-                log.timestamp < final
+        return if (comienzo == null && final == null) {
+            logs
+        }
+        else if(comienzo == null){
+            logs.filter{log->
+                log.timestamp?.let { timestamp-> !timestamp.isAfter(final) } ?: false
             }
         }
         else if(final == null){
-            logs.filter{log ->
-                log.timestamp > comienzo
+            logs.filter{log->
+                log.timestamp?.let { timestamp-> !timestamp.isBefore(comienzo) } ?: false
             }
         }
-        else{
-            logs.filter{log ->
-                log.timestamp > comienzo && log.timestamp < final
-            }
+        else {
+            logs.filter { it.timestamp?.let { timestamp-> !timestamp.isBefore(comienzo) && !timestamp.isAfter(final) } ?: false }
         }
     }
 }
