4c409e77728c95ce733eac6f78b614927cbed406
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-04-09 16:24:46 +0200

    Implementando método escribir datos en salida por archivo

--- src/main/kotlin/FileOutput.kt
+++ src/main/kotlin/FileOutput.kt
@@ -1,4 +1,14 @@
 package org.iesra
+import java.io.File
 
-class FileOutput : Output{
+class FileOutput(private val ruta : String) : Output{
+    override fun escribirDatos(contenido: String) {
+        val file = File(ruta)
+        try {
+            file.writeText(contenido)
+            println("Informe guardado con éxito en: ${file.absolutePath}")
+        } catch (e: Exception) {
+            println("Error: No se pudo escribir el fichero de salida: ${e.message}")
+        }
+    }
 }
