c3d98c86f43005f4583b60927f73a24b1e762914
ifonlob <ifonlob1108@g.educaand.es>
2026-04-10 09:29:37 +0200

    Lógica del procesador casi lista

--- src/main/kotlin/LogProcessor.kt
+++ src/main/kotlin/LogProcessor.kt
@@ -1,16 +1,31 @@
 package org.iesra
+import java.io.File
 import java.time.LocalDateTime
 
 class LogProcessor (private val parser : LogParser = LogParser(), private val filter : LogFilter = LogFilter(), private val stats : LogStats = LogStats()){
-    fun ejecutarProceso(lineasBrutas: List<String>, nombreArchivo: String, fechaInicio: LocalDateTime?, fechaFin: LocalDateTime?, soloEstadisticas: Boolean, salida: Output){
+    fun ejecutarProceso(lineasBrutas: List<String>, nombreArchivo: String, fechaInicio: LocalDateTime?, fechaFin: LocalDateTime?, soloEstadisticas: Boolean, salida : Output, niveles : List<LogLevel>?, ignorarInvalidos : Boolean){
+        val generador : TextGenerator
+
         val logsParseados = parser.parsear(lineasBrutas)
-        val logsFiltrados = filter.filtrar(logsParseados, fechaInicio, fechaFin)
-        val generador = TextGenerator(stats, logsFiltrados, nombreArchivo, fechaInicio, fechaFin)
+        val logsFiltradosFecha = filter.filtrarPorFechas(logsParseados, fechaInicio, fechaFin)
+        val logsFiltradosNiveles = filter.filtrarPorNiveles(logsParseados,niveles)
+
+        val logsFiltradosFinal = logsFiltradosFecha.intersect(logsFiltradosNiveles).toList()
+
+        if(ignorarInvalidos){
+            generador = TextGenerator(stats, logsFiltradosFinal.filter{it.isValid}, nombreArchivo, fechaInicio, fechaFin)
+        }
+        else{
+            generador = TextGenerator(stats, logsFiltradosFinal, nombreArchivo, fechaInicio, fechaFin)
+        }
+
         val contenidoFinal = if (soloEstadisticas) {
             generador.generarEstadisticas()
-        } else {
+        }
+        else {
             generador.generarReporte()
         }
+
         salida.escribirDatos(contenidoFinal)
     }
 }
