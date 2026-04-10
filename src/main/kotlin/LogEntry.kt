a95ae3acb4dadbf7dd5132342e2c504b2780b144
ifonlob <ifonlob1108@g.educaand.es>
2026-04-10 10:54:51 +0200

    Sobrescritura del método toString()

--- src/main/kotlin/LogEntry.kt
+++ src/main/kotlin/LogEntry.kt
@@ -1,10 +1,15 @@
 package org.iesra
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 data class LogEntry(
     val timestamp: LocalDateTime?,
     val level: LogLevel?,
     val message: String,
     val isValid: Boolean = true
-)
+){
+    private val formatoFecha = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    override fun toString(): String = "[${timestamp?.format(formatoFecha)}] $level $message"
+
+}
